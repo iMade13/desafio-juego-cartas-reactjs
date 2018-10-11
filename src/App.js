f67e7856373d729f27b13cff06c1a2a960bb1e26
imade13 <madeleine1302@gmail.com>
2018-10-11 16:21:13 -0300

    comparando cartas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import Header from './Components/Header/Header';
-import './App.css';
 import Board from './Components/Board/Board';
+import buildCard from './Components/Utils/buildCards';
+
+//Style
+import './App.css';
+
+const getInitialState = () => {
+  const naipe = buildCard();
+  return {
+    naipe,
+    pairSelected: [],
+    comparison: false
+  };
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getInitialState();
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <Board />
+        <Board 
+           card={this.state.naipe}
+           pairSelected={this.state.pairSelected}
+           cardSelection={(card) => this.cardSelection(card)}
+           />
       </div>
     );
+  }
+
+  cardSelection(card) {
+    if (
+      this.state.comparison ||
+      this.state.pairSelected.indexOf(card) > -1 ||
+      card.guessedCard
+    ){
+      return;
+    }
+
+    const pairSelected = [...this.state.pairSelected, card];
+    this.setState({
+      pairSelected 
+    })
+
   }
 }
 
